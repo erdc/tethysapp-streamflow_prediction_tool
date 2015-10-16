3b315187d06a4d18c410ac840a254746a8d482ad
snowman2 <alan.d.snow@byu.edu>
2015-10-16 11:08:55 -0500

    Updated highcharts to be highstock chart.

--- tethysapp/streamflow_prediction_tool/public/vendor/highcharts/export-csv.js
+++ tethysapp/streamflow_prediction_tool/public/vendor/highcharts/export-csv.js
@@ -3,7 +3,7 @@
  *
  * Author:   Torstein Honsi
  * Licence:  MIT
- * Version:  1.3.1
+ * Version:  1.3.6
  */
 /*global Highcharts, window, document, Blob */
 (function (Highcharts) {
@@ -12,6 +12,13 @@
 
     var each = Highcharts.each,
         downloadAttrSupported = document.createElement('a').download !== undefined;
+
+    Highcharts.setOptions({
+        lang: {
+            downloadCSV: 'Download CSV',
+            downloadXLS: 'Download XLS'
+        }
+    });
 
 
     /**
@@ -28,27 +35,49 @@
             x,
 
             // Options
-            dateFormat = options.dateFormat || '%Y-%m-%d %H:%M:%S';
+            dateFormat = options.dateFormat || '%Y-%m-%d %H:%M:%S',
+            columnHeaderFormatter = options.columnHeaderFormatter || function (series, key, keyLength) {
+                return series.name + (keyLength > 1 ? ' ('+ key + ')' : '');
+            };
 
         // Loop the series and index values
         i = 0;
         each(this.series, function (series) {
-            if (series.options.includeInCSVExport !== false) {
-                names.push(series.name);
-                each(series.points, function (point) {
-                    if (!rows[point.x]) {
-                        rows[point.x] = [];
+            var keys = series.options.keys,
+                pointArrayMap = keys || series.pointArrayMap || ['y'],
+                valueCount = pointArrayMap.length,
+                requireSorting = series.requireSorting,
+                j;
+
+            if (series.options.includeInCSVExport !== false && series.visible !== false) { // #55
+                j = 0;
+                while (j < valueCount) {
+                    names.push(columnHeaderFormatter(series, pointArrayMap[j], pointArrayMap.length));
+                    j = j + 1;
+                }
+
+                each(series.points, function (point, pIdx) {
+                    var key = requireSorting ? point.x : pIdx;
+
+                    j = 0;
+
+                    if (!rows[key]) {
+                        rows[key] = [];
                     }
-                    rows[point.x].x = point.x;
+                    rows[key].x = point.x;
 
                     // Pies, funnels etc. use point name in X row
                     if (!series.xAxis) {
-                        rows[point.x].name = point.name;
+                        rows[key].name = point.name;
                     }
 
-                    rows[point.x][i] = point.y;
+                    while (j < valueCount) {
+                        rows[key][i + j] = point[pointArrayMap[j]];
+                        j = j + 1;
+                    }
+
                 });
-                i += 1;
+                i = i + j;
             }
         });
 
@@ -69,8 +98,19 @@
         // Transform the rows to CSV
         each(rowArr, function (row) {
 
+            var category = row.name;
+            if (!category) {
+                if (xAxis.isDatetimeAxis) {
+                    category = Highcharts.dateFormat(dateFormat, row.x);
+                } else if (xAxis.categories) {
+                    category = Highcharts.pick(xAxis.names[row.x], xAxis.categories[row.x], row.x)
+                } else {
+                    category = row.x;
+                }
+            }
+
             // Add the X/date/category
-            row.unshift(row.name || (xAxis.isDatetimeAxis ? Highcharts.dateFormat(dateFormat, row.x) : xAxis.categories ? Highcharts.pick(xAxis.categories[row.x], row.x) : row.x));
+            row.unshift(category);
             dataRows.push(row);
         });
 
@@ -130,7 +170,7 @@
                 n = useLocalDecimalPoint ? (1.1).toLocaleString()[1] : '.';
 
             html += '<tr>';
-            for (j = 0; j < row.length; j++) {
+            for (j = 0; j < row.length; j = j + 1) {
                 val = row[j];
                 // Add the cell
                 if (typeof val === 'number') {
@@ -150,73 +190,98 @@
         return html;
     };
 
-    function getContent(chart, href, extention, content, MIME) {
+    function getContent(chart, href, extension, content, MIME) {
         var a,
             blobObject,
-            name = (chart.title ? chart.title.textStr.replace(/ /g, '-').toLowerCase() : 'chart');
+            name,
+            options = (chart.options.exporting || {}).csv || {},
+            url = options.url || 'http://www.highcharts.com/studies/csv-export/download.php';
+
+        if (chart.options.exporting.filename) {
+            name = chart.options.exporting.filename;
+        } else if (chart.title) {
+            name = chart.title.textStr.replace(/ /g, '-').toLowerCase();
+        } else {
+            name = 'chart';
+        }
+
         // Download attribute supported
         if (downloadAttrSupported) {
             a = document.createElement('a');
             a.href = href;
             a.target      = '_blank';
-            a.download    = name + '.' + extention;
+            a.download    = name + '.' + extension;
             document.body.appendChild(a);
             a.click();
             a.remove();
 
         } else if (window.Blob && window.navigator.msSaveOrOpenBlob) {
-            //Falls to msSaveOrOpenBlob if download attribute is not supported
+            // Falls to msSaveOrOpenBlob if download attribute is not supported
             blobObject = new Blob([content]);
-            window.navigator.msSaveOrOpenBlob(blobObject, name + '.' + extention);
+            window.navigator.msSaveOrOpenBlob(blobObject, name + '.' + extension);
+
         } else {
             // Fall back to server side handling
-            Highcharts.post('http://www.highcharts.com/studies/csv-export/download.php', {
+            Highcharts.post(url, {
                 data: content,
                 type: MIME,
-                extension: extention
+                extension: extension
             });
         }
     }
+
+    /**
+     * Call this on click of 'Download CSV' button
+     */
+    Highcharts.Chart.prototype.downloadCSV = function () {
+        var csv = this.getCSV(true);
+        getContent(
+            this,
+            'data:text/csv,' + csv.replace(/\n/g, '%0A'),
+            'csv',
+            csv,
+            'text/csv'
+        );
+    };
+
+    /**
+     * Call this on click of 'Download XLS' button
+     */
+    Highcharts.Chart.prototype.downloadXLS = function () {
+        var uri = 'data:application/vnd.ms-excel;base64,',
+            template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40">' +
+                '<head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet>' +
+                '<x:Name>Ark1</x:Name>' +
+                '<x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]-->' +
+                '<style>td{border:none;font-family: Calibri, sans-serif;} .number{mso-number-format:"0.00";}</style>' +
+                '<meta name=ProgId content=Excel.Sheet>' +
+                '</head><body>' +
+                this.getTable(true) +
+                '</body></html>',
+            base64 = function (s) { 
+                return window.btoa(unescape(encodeURIComponent(s))); // #50
+            };
+        getContent(
+            this,
+            uri + base64(template),
+            'xls',
+            template,
+            'application/vnd.ms-excel'
+        );
+    };
+
 
     // Add "Download CSV" to the exporting menu. Use download attribute if supported, else
     // run a simple PHP script that returns a file. The source code for the PHP script can be viewed at
     // https://raw.github.com/highslide-software/highcharts.com/master/studies/csv-export/csv.php
     if (Highcharts.getOptions().exporting) {
         Highcharts.getOptions().exporting.buttons.contextButton.menuItems.push({
-            text: Highcharts.getOptions().lang.downloadCSV || 'Download CSV',
-            onclick: function () {
-                var csv = this.getCSV(true);
-                getContent(
-                    this,
-                    'data:text/csv,' + csv.replace(/\n/g, '%0A'),
-                    'csv',
-                    csv,
-                    'text/csv'
-                );
-            }
-
+            textKey: 'downloadCSV',
+            onclick: function () { this.downloadCSV(); }
         }, {
-            text: Highcharts.getOptions().lang.downloadXLS || 'Download XLS',
-            onclick: function () {
-                var uri = 'data:application/vnd.ms-excel;base64,',
-                    template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40">' +
-                        '<head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet>' +
-                        '<x:Name>Ark1</x:Name>' +
-                        '<x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]-->' +
-                        '<style>td{border:none;font-family: Calibri, sans-serif;} .number{mso-number-format:"0.00";}</style>' +
-                        '<meta name=ProgId content=Excel.Sheet>' +
-                        '</head><body>' +
-                        this.getTable(true) +
-                        '</body></html>',
-                    base64 = function (s) { return window.btoa(decodeURIComponent(encodeURIComponent(s))); };
-                getContent(
-                    this,
-                    uri + base64(template),
-                    'xls',
-                    template,
-                    'application/vnd.ms-excel'
-                );
-            }
+            textKey: 'downloadXLS',
+            onclick: function () { this.downloadXLS(); }
         });
     }
+
 }(Highcharts));
