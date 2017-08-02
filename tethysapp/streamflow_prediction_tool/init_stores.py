9158049c1e5d1e57b20258d7b647437a7f1b49e0
snowman2 <alansnow21@gmail.com>
2017-08-02 09:12:30 -0500

    updated initialization to new settings and database methods

--- tethysapp/streamflow_prediction_tool/init_stores.py
+++ tethysapp/streamflow_prediction_tool/init_stores.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
-##
-##  init_stores.py
-##  streamflow_prediction_tool
-##
-##  Created by Alan D. Snow.
-##  Copyright © 2015-2016 Alan D. Snow. All rights reserved.
-##  License: BSD 3-Clause
+#
+#  init_stores.py
+#  streamflow_prediction_tool
+#
+#  Created by Alan D. Snow, 2015-2017
+#  License: BSD 3-Clause
 
 from .app import StreamflowPredictionTool as app
-from .model import Base, BaseLayer, DataStore, DataStoreType, MainSettings
+from .model import Base, DataStore, DataStoreType
 
 
 def init_main_db(engine, first_time):
@@ -17,25 +16,19 @@ def init_main_db(engine, first_time):
     
     # Initial data
     if first_time:
-        #make session
-        session_maker = app.get_persistent_store_database('main_db', as_sessionmaker=True)
+        # make session
+        session_maker = \
+            app.get_persistent_store_database('main_db',
+                                              as_sessionmaker=True)
         session = session_maker()
 
-        #add all possible base layers
-        session.add(BaseLayer("MapQuest","none",))
-        session.add(BaseLayer("Esri","none",))
-        session.add(BaseLayer("BingMaps","",))
-        
-        #add all possible data story types
+        # add all possible data story types
         session.add(DataStoreType("local", "Computations Local (None)"))
         session.add(DataStoreType("ckan", "CKAN"))
         session.add(DataStoreType("hydroshare", "HydroShare"))
         
-        #add all possible data stores
+        # add all possible data stores
         session.add(DataStore("Local Server", "", 1, "local", ""))
 
-        #add main settings
-        session.add(MainSettings(1, "", "", ""))
-        
         session.commit()
         session.close()
