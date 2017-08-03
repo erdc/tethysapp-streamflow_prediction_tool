8fcc3b375cfbef43a880aa98f6541cc7cc3af853
snowman2 <alansnow21@gmail.com>
2017-08-03 16:26:48 -0500

    passed flake8 & pylint

--- tethysapp/streamflow_prediction_tool/model.py
+++ tethysapp/streamflow_prediction_tool/model.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
-##
-##  model.py
-##  streamflow_prediction_tool
-##
-##  Created by Alan D. Snow.
-##  Copyright © 2015-2016 Alan D Snow. All rights reserved.
-##  License: BSD 3-Clause
+"""model.py
 
+    Created by Alan D. Snow, 2015-2017.
+    License: BSD 3-Clause
+"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
@@ -25,7 +22,7 @@ class DataStore(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     owner_org = Column(String)
-    data_store_type_id = Column(Integer,ForeignKey('data_store_type.id'))
+    data_store_type_id = Column(Integer, ForeignKey('data_store_type.id'))
     data_store_type = relationship("DataStoreType")
     api_endpoint = Column(String)
     api_key = Column(String)
@@ -71,7 +68,7 @@ class GeoServerLayer(Base):
     projection = Column(String, default="")
     attribute_list = Column(String, default="")
     wfs_url = Column(String, default="")
-    
+
 
 class Watershed(Base):
     """
@@ -83,11 +80,16 @@ class Watershed(Base):
     id = Column(Integer, primary_key=True)
     data_store_id = Column(Integer, ForeignKey('data_store.id'))
     geoserver_id = Column(Integer, ForeignKey('geoserver.id'))
-    geoserver_drainage_line_layer_id = Column(Integer, ForeignKey('geoserver_layer.id'))
-    geoserver_boundary_layer_id = Column(Integer, ForeignKey('geoserver_layer.id'))
-    geoserver_gage_layer_id = Column(Integer, ForeignKey('geoserver_layer.id'))
-    geoserver_historical_flood_map_layer_id = Column(Integer, ForeignKey('geoserver_layer.id'))
-    geoserver_ahps_station_layer_id = Column(Integer, ForeignKey('geoserver_layer.id'))
+    geoserver_drainage_line_layer_id = \
+        Column(Integer, ForeignKey('geoserver_layer.id'))
+    geoserver_boundary_layer_id = \
+        Column(Integer, ForeignKey('geoserver_layer.id'))
+    geoserver_gage_layer_id = \
+        Column(Integer, ForeignKey('geoserver_layer.id'))
+    geoserver_historical_flood_map_layer_id = \
+        Column(Integer, ForeignKey('geoserver_layer.id'))
+    geoserver_ahps_station_layer_id = \
+        Column(Integer, ForeignKey('geoserver_layer.id'))
     watershed_name = Column(String)
     subbasin_name = Column(String)
     watershed_clean_name = Column(String)
@@ -97,26 +99,31 @@ class Watershed(Base):
     ecmwf_data_store_watershed_name = Column(String)
     ecmwf_data_store_subbasin_name = Column(String)
     geoserver = relationship("Geoserver")
-    geoserver_drainage_line_layer = relationship("GeoServerLayer", 
-                                                 foreign_keys=[geoserver_drainage_line_layer_id],
-                                                 single_parent=True,
-                                                 cascade="save-update,merge,delete,delete-orphan")
-    geoserver_boundary_layer = relationship("GeoServerLayer", 
-                                            foreign_keys=[geoserver_boundary_layer_id],
-                                            single_parent=True,
-                                            cascade="save-update,merge,delete,delete-orphan")
-    geoserver_gage_layer = relationship("GeoServerLayer", 
-                                        foreign_keys=[geoserver_gage_layer_id],
-                                        single_parent=True,
-                                        cascade="save-update,merge,delete,delete-orphan")
-    geoserver_historical_flood_map_layer = relationship("GeoServerLayer", 
-                                                        foreign_keys=[geoserver_historical_flood_map_layer_id],
-                                                        single_parent=True,
-                                                        cascade="save-update,merge,delete,delete-orphan")
-    geoserver_ahps_station_layer = relationship("GeoServerLayer", 
-                                                foreign_keys=[geoserver_ahps_station_layer_id],
-                                                single_parent=True,
-                                                cascade="save-update,merge,delete,delete-orphan")
+    geoserver_drainage_line_layer = \
+        relationship("GeoServerLayer",
+                     foreign_keys=[geoserver_drainage_line_layer_id],
+                     single_parent=True,
+                     cascade="save-update,merge,delete,delete-orphan")
+    geoserver_boundary_layer = \
+        relationship("GeoServerLayer",
+                     foreign_keys=[geoserver_boundary_layer_id],
+                     single_parent=True,
+                     cascade="save-update,merge,delete,delete-orphan")
+    geoserver_gage_layer = \
+        relationship("GeoServerLayer",
+                     foreign_keys=[geoserver_gage_layer_id],
+                     single_parent=True,
+                     cascade="save-update,merge,delete,delete-orphan")
+    geoserver_historical_flood_map_layer = \
+        relationship("GeoServerLayer",
+                     foreign_keys=[geoserver_historical_flood_map_layer_id],
+                     single_parent=True,
+                     cascade="save-update,merge,delete,delete-orphan")
+    geoserver_ahps_station_layer = \
+        relationship("GeoServerLayer",
+                     foreign_keys=[geoserver_ahps_station_layer_id],
+                     single_parent=True,
+                     cascade="save-update,merge,delete,delete-orphan")
     watershed_groups = relationship("WatershedGroup",
                                     secondary='watershed_watershed_group_link')
 
@@ -126,8 +133,10 @@ class WatershedWatershedGroupLink(Base):
     SQLAlchemy many-to-many link between watershed and watershed_group
     """
     __tablename__ = 'watershed_watershed_group_link'
-    watershed_group_id = Column(Integer, ForeignKey('watershed_group.id'), primary_key=True)
-    watershed_id = Column(Integer, ForeignKey('watershed.id'), primary_key=True)
+    watershed_group_id = \
+        Column(Integer, ForeignKey('watershed_group.id'), primary_key=True)
+    watershed_id = \
+        Column(Integer, ForeignKey('watershed.id'), primary_key=True)
 
 
 class WatershedGroup(Base):
@@ -139,5 +148,5 @@ class WatershedGroup(Base):
     # Columns
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    watersheds = relationship("Watershed", 
+    watersheds = relationship("Watershed",
                               secondary='watershed_watershed_group_link')
