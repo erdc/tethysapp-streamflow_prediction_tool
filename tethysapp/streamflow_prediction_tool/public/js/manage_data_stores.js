cfbe4ccfc2356c91f2d763b09a58c2e4eeb4a141
snowman2 <alansnow21@gmail.com>
2017-08-10 15:22:52 -0500

    updated data store management to use datatables

--- tethysapp/streamflow_prediction_tool/public/js/manage_data_stores.js
+++ tethysapp/streamflow_prediction_tool/public/js/manage_data_stores.js
@@ -17,26 +17,29 @@ var ERFP_MANAGE_DATA_STORES = (function() {
     /************************************************************************
     *                      MODULE LEVEL / GLOBAL VARIABLES
     *************************************************************************/
-    var m_uploading_data, m_results_per_page;
+    var m_uploading_data;
 
     /************************************************************************
      *                    PRIVATE FUNCTION DECLARATIONS
      *************************************************************************/
-    var initializeTableFunctions, getTablePage, displayResultsText;
+    var initializeTableFunctions, getTablePage;
 
 
     /************************************************************************
      *                    PRIVATE FUNCTION IMPLEMENTATIONS
      *************************************************************************/
-
-    m_results_per_page = 5;
-
     initializeTableFunctions = function() {
-        
+        $("#data_store_table").DataTable({
+            destroy: true,
+            columnDefs: [{
+                orderable: false,
+                targets: [0, 1]
+            }],
+            order: [[ 3, "asc" ]]
+        });
+
         //handle the submit update event
         $('.submit-update-data-store').off().click(function(){
-            //scroll back to top
-            window.scrollTo(0,0);
             //clear messages
             $('#message').addClass('hidden');
             $('#message').empty()
@@ -64,9 +67,7 @@ var ERFP_MANAGE_DATA_STORES = (function() {
             //update database
             var xhr = submitRowData($(this), data, safe_to_submit); 
             xhr.done(function(data) {
-                if ('success' in data) {
-                    addSuccessMessage("Data Store Successfully Updated!");
-                }
+                addSuccessMessage("Data Store Successfully Updated!");
             });
         });
         
@@ -80,74 +81,19 @@ var ERFP_MANAGE_DATA_STORES = (function() {
             var xhr = deleteRowData($(this), data);
             if (xhr != null) {
                 xhr.done(function (data) {
-                    if('success' in data) {
-                        var num_data_stores_data = $('#manage_data_stores_table').data('num_data_stores');
-                        var page = parseInt($('#manage_data_stores_table').data('page'));
-                        $('#manage_data_stores_table').data('num_data_stores', Math.max(0, parseInt(num_data_stores_data) - 1));
-                        if (parseInt($('#manage_data_stores_table').data('num_data_stores')) <= m_results_per_page * page) {
-                            $('#manage_data_stores_table').data('page', Math.max(0, page - 1));
-                        }
-                        getTablePage();
-                        addSuccessMessage("Data Store Successfully Deleted!");
-                    }
+                    getTablePage();
+                    addSuccessMessage("Data Store Successfully Deleted!");
                 });
             }
         });
 
-        displayResultsText();
-        if (m_results_per_page >= $('#manage_data_stores_table').data('num_data_stores')) {
-            $('[name="prev_button"]').addClass('hidden');
-            $('[name="next_button"]').addClass('hidden');
-        }
-
-        //pagination next and previous button update
-        $('[name="prev_button"]').click(function(){
-            var page = parseInt($('#manage_data_stores_table').data('page'));
-            $('#manage_data_stores_table').data('page', Math.max(0, page-1));
-            getTablePage();
-        });
-        $('[name="next_button"]').click(function(){
-            var page = parseInt($('#manage_data_stores_table').data('page'));
-            $('#manage_data_stores_table').data('page', Math.min(page+1,
-                                                Math.floor(parseInt($('#manage_data_stores_table').data('num_data_stores')) / m_results_per_page - 0.1)));
-            getTablePage();
-        });
     };
 
-
-    displayResultsText = function() {
-        //dynamically show table results display info text on page
-        var page = parseInt($('#manage_data_stores_table').data('page'));
-        var num_data_stores_data = $('#manage_data_stores_table').data('num_data_stores');
-        var display_min;
-        if (num_data_stores_data == 0){
-            display_min = 0
-        }
-        else{
-            display_min = ((page + 1) * m_results_per_page) - (m_results_per_page - 1);
-        }
-        var display_max = Math.min(num_data_stores_data, ((page + 1) * m_results_per_page));
-        $('[name="prev_button"]').removeClass('hidden');
-        $('[name="next_button"]').removeClass('hidden');
-        if (page == 0){
-            $('[name="prev_button"]').addClass('hidden');
-        } else if (page == Math.floor(num_data_stores_data / m_results_per_page - 0.1)) {
-            $('[name="next_button"]').addClass('hidden');
-        }
-        if (num_data_stores_data != 0) {
-            $('#display-info').append('Displaying data stores ' + display_min + ' - ' +
-                display_max + ' of ' + num_data_stores_data);
-        }else {
-            $('#display-info').append('No data stores to display' + '<br>To add one, ' +
-                'click <a href="../add-data-store">here</a>.');
-        }
-    };
 
     getTablePage = function() {
         $.ajax({
             url: 'table',
             method: 'GET',
-            data: {'page': $('#manage_data_stores_table').data('page')},
             success: function(data) {
                 $("#manage_data_stores_table").html(data);
                 initializeTableFunctions();
@@ -162,6 +108,6 @@ var ERFP_MANAGE_DATA_STORES = (function() {
 
     $(function() {
         m_uploading_data = false;
-        getTablePage();
+        initializeTableFunctions();
     }); //document ready
 }()); // End of package wrapper
